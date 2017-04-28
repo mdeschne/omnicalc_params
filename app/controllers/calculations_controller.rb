2e9b896d577238da4f6fcb9c87a20332f2e55f5d
mdeschne <mdeschne@users.noreply.github.com>
2017-04-27 19:14:10 -0500

    finished

--- app/controllers/calculations_controller.rb
+++ app/controllers/calculations_controller.rb
@@ -18,15 +18,106 @@ def flex_root
   render("calculations/flex_root.html.erb")
 end
 
+
 def flex_pmt
-# 
-# @years  @pal @apr @pmt.
-#
-# @input = params["x"].to_f
-#
-# @pmt = @input ** 0.5
+
+@years =params["number_of_years"].to_f
+@principal = params["present_value"].to_f
+@apr = params["basis_points"].to_f
+
+@apr = @apr/100
+apr =  @apr.round(2)/100
+# rate = ((1+(apr/12))**12)-1
+rate = apr/12
+months = @years*12
+pal =  @principal
+
+pmt = (pal*rate)/(1-(1+rate)**(-months))
+
+
+  @pmt = pmt.round(2)
+
 
   render("calculations/flex_pmt.html.erb")
+end
+
+def flex_rand
+
+@min = params["min"].to_f
+@max = params["max"].to_f
+
+
+@rand = rand((@max-@min)) + @min
+
+  render("calculations/flex_rand.html.erb")
+end
+
+
+def square_form
+  render("calculations/square_form.html.erb")
+end
+
+def square
+
+  @input = params["x"].to_f
+
+  @square = @input ** 2
+
+    render("calculations/square.html.erb")
+end
+
+def square_root_form
+  render("calculations/square_root_form.html.erb")
+end
+
+def square_root
+
+  @input = params["x"].to_f
+
+  @root = @input ** 0.5
+
+    render("calculations/square_root.html.erb")
+end
+
+def payment_form
+  render("calculations/payment_form.html.erb")
+end
+
+def payment
+
+  @years =params["noy"].to_f
+  @principal = params["pv"].to_f
+  @apr = params["bp"].to_f
+
+  @apr = @apr
+  apr =  @apr.round(2)/100
+  # rate = ((1+(apr/12))**12)-1
+  rate = apr/12
+  months = @years*12
+  pal =  @principal
+
+  pmt = (pal*rate)/(1-(1+rate)**(-months))
+
+
+    @pmt = pmt.round(2)
+
+
+    render("calculations/payment.html.erb")
+end
+
+def random_form
+  render("calculations/random_form.html.erb")
+end
+
+def random
+
+@min = params["user_min"].to_f
+@max = params["user_max"].to_f
+
+
+@rand = rand((@max-@min)) + @min
+
+  render("calculations/random.html.erb")
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,20 @@ Rails.application.routes.draw do
 get("/square/new", {:controller => "calculations", :action => "square_form"})
 get("/square/results", {:controller => "calculations", :action => "square"})
 
+get("/square_root/new", {:controller => "calculations", :action => "square_root_form"})
+get("/square_root/results", {:controller => "calculations", :action => "square_root"})
+
+get("/payment/new", {:controller => "calculations", :action => "payment_form"})
+get("/payment/results", {:controller => "calculations", :action => "payment"})
+
+get("/random/new", {:controller => "calculations", :action => "random_form"})
+get("/random/results", {:controller => "calculations", :action => "random"})
+
 get("/flexible/square/:x", { :controller => "calculations", :action => "flex_square"})
 
 get("/flexible/square_root/:x", { :controller => "calculations", :action => "flex_root"})
 
-get("/flexible/payment/:x/:y/:z", { :controller => "calculations", :action => "flex_pmt"})
+get("/flexible/payment/:basis_points/:number_of_years/:present_value", { :controller => "calculations", :action => "flex_pmt"})
 
 get("/flexible/random/:min/:max", { :controller => "calculations", :action => "flex_rand"})
 
